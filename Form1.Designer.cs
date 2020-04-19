b176dec6c6b9f79419f90ae3adfbf2719074448d
SRICHAI CHAISRI <iamsrichai@gmail.com>
2020-04-19 10:26:07 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace App7Segment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblG = new System.Windows.Forms.Label();
+            this.lblD = new System.Windows.Forms.Label();
+            this.lblF = new System.Windows.Forms.Label();
+            this.lblE = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCC = new System.Windows.Forms.Label();
+            this.lblBB = new System.Windows.Forms.Label();
+            this.lblEE = new System.Windows.Forms.Label();
+            this.lblFF = new System.Windows.Forms.Label();
+            this.lblDD = new System.Windows.Forms.Label();
+            this.lblGG = new System.Windows.Forms.Label();
+            this.lblAA = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblA.Location = new System.Drawing.Point(130, 125);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(150, 30);
+            this.lblA.TabIndex = 0;
+            // 
+            // lblG
+            // 
+            this.lblG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblG.Location = new System.Drawing.Point(130, 260);
+            this.lblG.Name = "lblG";
+            this.lblG.Size = new System.Drawing.Size(150, 30);
+            this.lblG.TabIndex = 1;
+            // 
+            // lblD
+            // 
+            this.lblD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblD.Location = new System.Drawing.Point(130, 395);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(150, 30);
+            this.lblD.TabIndex = 2;
+            // 
+            // lblF
+            // 
+            this.lblF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblF.Location = new System.Drawing.Point(100, 125);
+            this.lblF.Name = "lblF";
+            this.lblF.Size = new System.Drawing.Size(30, 150);
+            this.lblF.TabIndex = 3;
+            // 
+            // lblE
+            // 
+            this.lblE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblE.Location = new System.Drawing.Point(100, 275);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(30, 150);
+            this.lblE.TabIndex = 4;
+            // 
+            // lblC
+            // 
+            this.lblC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblC.Location = new System.Drawing.Point(280, 275);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(30, 150);
+            this.lblC.TabIndex = 6;
+            // 
+            // lblB
+            // 
+            this.lblB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblB.Location = new System.Drawing.Point(280, 125);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(30, 150);
+            this.lblB.TabIndex = 5;
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.btn.Location = new System.Drawing.Point(635, 12);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(153, 58);
+            this.btn.TabIndex = 7;
+            this.btn.Text = "CLICK";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCC
+            // 
+            this.lblCC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblCC.Location = new System.Drawing.Point(527, 275);
+            this.lblCC.Name = "lblCC";
+            this.lblCC.Size = new System.Drawing.Size(30, 150);
+            this.lblCC.TabIndex = 14;
+            // 
+            // lblBB
+            // 
+            this.lblBB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblBB.Location = new System.Drawing.Point(527, 125);
+            this.lblBB.Name = "lblBB";
+            this.lblBB.Size = new System.Drawing.Size(30, 150);
+            this.lblBB.TabIndex = 13;
+            // 
+            // lblEE
+            // 
+            this.lblEE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblEE.Location = new System.Drawing.Point(347, 275);
+            this.lblEE.Name = "lblEE";
+            this.lblEE.Size = new System.Drawing.Size(30, 150);
+            this.lblEE.TabIndex = 12;
+            // 
+            // lblFF
+            // 
+            this.lblFF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblFF.Location = new System.Drawing.Point(347, 125);
+            this.lblFF.Name = "lblFF";
+            this.lblFF.Size = new System.Drawing.Size(30, 150);
+            this.lblFF.TabIndex = 11;
+            // 
+            // lblDD
+            // 
+            this.lblDD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblDD.Location = new System.Drawing.Point(377, 395);
+            this.lblDD.Name = "lblDD";
+            this.lblDD.Size = new System.Drawing.Size(150, 30);
+            this.lblDD.TabIndex = 10;
+            // 
+            // lblGG
+            // 
+            this.lblGG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblGG.Location = new System.Drawing.Point(377, 260);
+            this.lblGG.Name = "lblGG";
+            this.lblGG.Size = new System.Drawing.Size(150, 30);
+            this.lblGG.TabIndex = 9;
+            // 
+            // lblAA
+            // 
+            this.lblAA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblAA.Location = new System.Drawing.Point(377, 125);
+            this.lblAA.Name = "lblAA";
+            this.lblAA.Size = new System.Drawing.Size(150, 30);
+            this.lblAA.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCC);
+            this.Controls.Add(this.lblBB);
+            this.Controls.Add(this.lblEE);
+            this.Controls.Add(this.lblFF);
+            this.Controls.Add(this.lblDD);
+            this.Controls.Add(this.lblGG);
+            this.Controls.Add(this.lblAA);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.lblF);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.lblG);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblG;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.Label lblF;
+        private System.Windows.Forms.Label lblE;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCC;
+        private System.Windows.Forms.Label lblBB;
+        private System.Windows.Forms.Label lblEE;
+        private System.Windows.Forms.Label lblFF;
+        private System.Windows.Forms.Label lblDD;
+        private System.Windows.Forms.Label lblGG;
+        private System.Windows.Forms.Label lblAA;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App7Segment
+{
+    public partial class Form1 : Form
+    {
+        int num = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Display1(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    lblA.BackColor = Color.Red;
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblE.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+
+                    lblG.BackColor = Color.Black;
+                    break;
+                case 1:
+
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+
+                    lblA.BackColor = Color.Black;
+                    lblD.BackColor = Color.Black;
+                    lblE.BackColor = Color.Black;
+                    lblF.BackColor = Color.Black;
+                    lblG.BackColor = Color.Black;
+                    break;
+                case 2:
+                    lblA.BackColor = Color.Red;
+                    lblB.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblE.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+
+                    lblC.BackColor = Color.Black;
+                    lblF.BackColor = Color.Black;
+
+
+                    break;
+                case 3:
+                    lblA.BackColor = Color.Red;
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+
+                    lblE.BackColor = Color.Black;
+                    lblF.BackColor = Color.Black;
+
+                    break;
+                case 4:
+
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+
+                    lblA.BackColor = Color.Black;
+                    lblE.BackColor = Color.Black;
+                    lblD.BackColor = Color.Black;
+                    break;
+                case 5:
+                    lblA.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+
+                    lblB.BackColor = Color.Black;
+                    lblE.BackColor = Color.Black;
+                    break;
+                case 6:
+                    lblA.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblE.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+
+                    lblB.BackColor = Color.Black;
+                    break;
+                case 7:
+                    lblA.BackColor = Color.Red;
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+
+                    lblD.BackColor = Color.Black;
+                    lblE.BackColor = Color.Black;
+                    lblG.BackColor = Color.Black;
+                    break;
+                case 8:
+                    lblA.BackColor = Color.Red;
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblE.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+                    break;
+                case 9:
+                    lblA.BackColor = Color.Red;
+                    lblB.BackColor = Color.Red;
+                    lblC.BackColor = Color.Red;
+                    lblD.BackColor = Color.Red;
+                    lblF.BackColor = Color.Red;
+                    lblG.BackColor = Color.Red;
+
+                    lblE.BackColor = Color.Black;
+                    break;
+                default:
+                    // code block
+                    break;
+            }
+        }
+
+        public void Display2(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    lblAA.BackColor = Color.Red;
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblEE.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+
+                    lblGG.BackColor = Color.Black;
+                    break;
+                case 1:
+
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+
+                    lblAA.BackColor = Color.Black;
+                    lblDD.BackColor = Color.Black;
+                    lblEE.BackColor = Color.Black;
+                    lblFF.BackColor = Color.Black;
+                    lblGG.BackColor = Color.Black;
+                    break;
+                case 2:
+                    lblAA.BackColor = Color.Red;
+                    lblBB.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblEE.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+
+                    lblCC.BackColor = Color.Black;
+                    lblFF.BackColor = Color.Black;
+
+
+                    break;
+                case 3:
+                    lblAA.BackColor = Color.Red;
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+
+                    lblEE.BackColor = Color.Black;
+                    lblFF.BackColor = Color.Black;
+
+                    break;
+                case 4:
+
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+
+                    lblAA.BackColor = Color.Black;
+                    lblEE.BackColor = Color.Black;
+                    lblDD.BackColor = Color.Black;
+                    break;
+                case 5:
+                    lblAA.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+
+                    lblBB.BackColor = Color.Black;
+                    lblEE.BackColor = Color.Black;
+                    break;
+                case 6:
+                    lblAA.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblEE.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+
+                    lblBB.BackColor = Color.Black;
+                    break;
+                case 7:
+                    lblAA.BackColor = Color.Red;
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+
+                    lblDD.BackColor = Color.Black;
+                    lblEE.BackColor = Color.Black;
+                    lblGG.BackColor = Color.Black;
+                    break;
+                case 8:
+                    lblAA.BackColor = Color.Red;
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblEE.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+                    break;
+                case 9:
+                    lblAA.BackColor = Color.Red;
+                    lblBB.BackColor = Color.Red;
+                    lblCC.BackColor = Color.Red;
+                    lblDD.BackColor = Color.Red;
+                    lblFF.BackColor = Color.Red;
+                    lblGG.BackColor = Color.Red;
+
+                    lblEE.BackColor = Color.Black;
+                    break;
+                default:
+                    // code block
+                    break;
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (num == 9)
+                num = -1;
+
+            if(num ==-1 || num<10)
+            {
+                num++;
+                Display1(num);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int i = (DateTime.Now.Second % 10);
+            int ii = (DateTime.Now.Second / 10);
+            Display1(ii);
+            Display2(i);
+        }
+    }
+}
